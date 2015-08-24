6c23837b4be30ff50d36663acbdacc8e41aff806
mac <mac@macdeMacBook-Pro.local>
2015-08-24 21:18:40 +0800

    Dropit swift 2.0

---
+++ Dropit/DropitBehavior.swift
@@ -0,0 +1,53 @@
+//
+//  DropitBehavior.swift
+//  Dropit
+//
+//  Created by mac on 15/8/24.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+import UIKit
+
+class DropitBehavior: UIDynamicBehavior {
+    let gravity = UIGravityBehavior()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedDynamicAnimator = UICollisionBehavior()
+        lazilyCreatedDynamicAnimator.translatesReferenceBoundsIntoBoundary = true
+        return lazilyCreatedDynamicAnimator
+        }()
+    
+    lazy var dropBehavior: UIDynamicItemBehavior = {
+        let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        lazilyCreatedDropBehavior.allowsRotation = true
+        lazilyCreatedDropBehavior.elasticity = 0.75
+        return lazilyCreatedDropBehavior
+    }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
+    }
+    
+    func addBarrier(path: UIBezierPath, named name: String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    
+    func addDrop(drop: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(drop)
+        gravity.addItem(drop)
+        collider.addItem(drop)
+        dropBehavior.addItem(drop)
+    }
+    
+    func removeDrop(drop: UIView) {
+        gravity.removeItem(drop)
+        collider.removeItem(drop)
+        dropBehavior.removeItem(drop)
+        drop.removeFromSuperview()
+    }
+
+}
